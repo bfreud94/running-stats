724dcac96c6d00428f3de7083b1cbd08c81fed1e
Ben Freudman <bfreud94@gmail.com>
2025-03-02 23:37:22 -0500

    corrected insert query

--- src/gulp/dto.js
+++ src/gulp/dto.js
@@ -1,16 +1,16 @@
 import { metersToMile } from './util.js'
 
 export const activityDtoToSqlStatement = (activityDto, isFirstElement, isLastElement) =>
-	`${isFirstElement ? '),' : ''}(${activityDto.average_cadence},${activityDto.average_heartrate},${activityDto.average_speed},` +
-	`${metersToMile(activityDto.distance)},${activityDto.id},'${activityDto.name}','${activityDto.start_date}',` +
+	`${isFirstElement ? '),' : ''}('${activityDto.id}',${activityDto.average_cadence},${activityDto.average_heartrate},${activityDto.average_speed},` +
+	`${activityDto.distance},'${activityDto.name}','${activityDto.start_date}',` +
 	`${activityDto.suffer_score},${activityDto.time})${isLastElement ? '' : ','}`
 
 export const createActivityDto = (activity) => ({
+	id: activity.id,
 	average_cadence: activity.average_cadence || 0,
 	average_heartrate: activity.average_heartrate || 0,
 	average_speed: activity.average_speed,
 	distance: metersToMile(activity.distance),
-	id: activity.id,
 	name: activity.name,
 	start_date: formatActivityDtoDate(activity.start_date_local),
 	suffer_score: activity.suffer_score || 0,
@@ -20,11 +20,11 @@ export const createActivityDto = (activity) => ({
 export const formatActivityDtoDate = (date) => new Date(date).toISOString().slice(0, 19).replace('T', ' ')
 
 export const getActivityDtoValues = (activityDbto) => [
+	activityDbto.id,
 	activityDbto.average_cadence,
 	activityDbto.average_heartrate,
 	activityDbto.average_speed,
 	activityDbto.distance,
-	activityDbto.id,
 	activityDbto.name,
 	activityDbto.start_date,
 	activityDbto.suffer_score,
